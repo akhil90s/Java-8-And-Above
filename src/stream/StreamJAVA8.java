d9666a3de4064305b2438f5cb557072285b37817
akhil90s <akhil.chawla2010@gmail.com>
2021-08-10 22:08:16 +0530

    JAVA8

--- src/stream/StreamJAVA8.java
+++ src/stream/StreamJAVA8.java
@@ -14,72 +14,66 @@ public class StreamJAVA8 {
 
 	public static void main(String[] args) {
 
-		List<Integer> listOfIntegers = getListOfInteger();
+		List<Integer> listOfIntegers = new ArrayList<>();
+		Collections.addAll(listOfIntegers, 11, 3, 5, 20, 4, 3, 21, 45, 98, 21, 98);
 
 		// Find out sum of all integers greater than 10
 		int sumOfAllIntegersGreaterThan10 = listOfIntegers.stream().filter((Integer i) -> i > 10).mapToInt(i -> i)
 				.sum();
 		System.out.println(sumOfAllIntegersGreaterThan10);
-		
+
 		// Find duplicate elements in a list using stream functions
 		Set<Integer> set = new HashSet<>();
-		List<Integer> dublicateElements = listOfIntegers.stream().filter(n->!set.add(n)).collect(Collectors.toList());
+		List<Integer> dublicateElements = listOfIntegers.stream().filter(n -> !set.add(n)).collect(Collectors.toList());
 		System.out.println(dublicateElements);
-		
-		// Find the total number of all the elements 
+
+		// Find the total number of all the elements
 		long countOfElements = listOfIntegers.stream().count();
 		System.out.println(countOfElements);
-		
+
 		// Find first non-repeated character in the string
 		String input = "Java Hungry Blog Alive is Awesome";
-		Character resultNonRepeatedCharacter = input.chars().mapToObj(s -> Character.toLowerCase(Character.valueOf((char) s)))
+		Character resultNonRepeatedCharacter = input.chars()
+				.mapToObj(s -> Character.toLowerCase(Character.valueOf((char) s)))
 				.collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
 				.entrySet().stream().filter(entry -> entry.getValue() == 1L).map(entry -> entry.getKey()).findFirst()
 				.get();
 		System.out.println(resultNonRepeatedCharacter);
 
 		// Find first repeated character in the string
-		Character resultFirstRepeatedCharacter = input.chars().mapToObj(s -> Character.toLowerCase(Character.valueOf((char) s)))
+		Character resultFirstRepeatedCharacter = input.chars()
+				.mapToObj(s -> Character.toLowerCase(Character.valueOf((char) s)))
 				.collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
 				.entrySet().stream().filter(entry -> entry.getValue() > 1L).map(entry -> entry.getKey()).findFirst()
 				.get();
 		System.out.println(resultFirstRepeatedCharacter);
-		
+
 		// Find the names from a list of names having length more than 3
 		List<String> listOfNames = new ArrayList<>();
 		Collections.addAll(listOfNames, "Akhil", "Amitesh", "Akshay", "Anirudh", "Milind");
 		List<String> filteredListOfNames = listOfNames.stream().filter(s -> s.length() > 3)
 				.collect(Collectors.toList());
 		System.out.println(filteredListOfNames);
+
 		
-		
-		// Find the youngest individual from a group of people
 		List<Individual> listOfIndividuals = new ArrayList<>();
-		
+
 		Individual i1 = new Individual("Akhil", 25);
 		Individual i2 = new Individual("Amitesh", 23);
 		Individual i3 = new Individual("Akshay", 25);
 		Individual i4 = new Individual("Anirudh", 27);
 		Individual i5 = new Individual("Milind", 21);
-		
+
 		Collections.addAll(listOfIndividuals, i1, i2, i3, i4, i5);
-		
-	    Individual youngestPerson = listOfIndividuals.stream().min(Comparator.comparing(Individual::getAge)).get();
+
+		// Find the youngest individual from a group of people
+		Individual youngestPerson = listOfIndividuals. stream().min(Comparator.comparing(Individual::getAge)).get();
 		System.out.println(youngestPerson);
-		
+
 		// Find the oldest individual from a group of people
-		
 		Individual oldestPerson = listOfIndividuals.stream().max(Comparator.comparing(Individual::getAge)).get();
 		System.out.println(oldestPerson);
-			
-		
-	}
-	
-	static List<Integer> getListOfInteger() {
 
-		List<Integer> listOfInteger = new ArrayList<>();
-		Collections.addAll(listOfInteger, 11, 3, 5, 20, 4, 3, 21, 45, 98, 21, 98);
-		return listOfInteger;
 	}
 
 }
@@ -110,11 +104,10 @@ class Individual {
 	public void setAge(int age) {
 		this.age = age;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Individual [name=" + name + ", age=" + age + "]";
 	}
 
 }
-
